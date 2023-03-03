44f1b40ec642699f6444ca3ab0bd18ec7c73233c
IndigoQuadratic <107479389+IndigoQuadratic@users.noreply.github.com>
2023-03-02 23:03:57 -0500

    Update IK_velocity.py

--- IK_velocity.py
+++ IK_velocity.py
@@ -24,5 +24,5 @@ def IK_velocity(q_in, v_in, omega_in):
     v_in = v_in.reshape((3,1))
     omega_in = omega_in.reshape((3,1))
     zeta = np.vstack((v_in,omega_in))
-    dq = lstsq(J,zeta)
+    dq,b = np.linalg.lstsq(J, zeta, rcond=None)[0]
     return dq
